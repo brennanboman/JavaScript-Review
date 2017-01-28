5e60cbe3a16686852b717fa234c4c0b9e9766956
Brennan Boman <brennanboman@Brennans-MacBook-Air.local>
2017-01-28 13:47:13 -0700

    first/second question done

--- arrayReview.js
+++ arrayReview.js
@@ -55,8 +55,9 @@ var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
 //Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this, first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
 var first = [1,2,3,4,5];
-var second;
+var second = first.slice()
 
+second.push(6, 7)
   //code here
 
 alert(first) //[1,2,3,4,5];
